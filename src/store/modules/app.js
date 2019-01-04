c05663ac79f960e1b16d337043e29b9dbc326e95
xiao-b <32984116+xiao-b@users.noreply.github.com>
2019-01-04 18:01:58 +0800

    commit

---
+++ src/store/modules/app.js
@@ -0,0 +1,11 @@
+import { Loading } from "element-ui";
+import { Message } from "element-ui";
+
+const app = {
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {}
+};
+
+export default app;
